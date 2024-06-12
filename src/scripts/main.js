65ec728d0a8d47fc7dfbc4a4b7fa387e32d878d5
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2024-06-12 13:10:08 +0300

    remove old style(comments):shadow, stroke, gap

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   modalCls.addEventListener("click", () => {
     clearForm();
+    clearErr();
     modal.classList.remove("show");
   });
 
